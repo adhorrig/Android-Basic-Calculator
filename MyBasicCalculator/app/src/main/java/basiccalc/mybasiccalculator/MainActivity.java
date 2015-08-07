4594f62d9ba32de896bbf01cddc410ff5999cce1
ADHORRIG <adhorrig@gmail.com>
2015-08-07 21:18:51 +0100

    Improved navigation.
    Buttons added to the action bar to allow the user to get to a new
    screen. ~Starting a new activity. New activity will be  used for the the
    bet type is double.

--- MyBasicCalculator/app/src/main/java/basiccalc/mybasiccalculator/MainActivity.java
+++ MyBasicCalculator/app/src/main/java/basiccalc/mybasiccalculator/MainActivity.java
@@ -1,12 +1,17 @@
 package basiccalc.mybasiccalculator;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
 
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener{
@@ -51,5 +56,48 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
                 }
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void openSearch(){
+        Toast.makeText(this, "Settings button pressed", Toast.LENGTH_SHORT).show();
+    }
+
+    private void openSettings(){
+        Toast.makeText(this, "Settings button pressed", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+
+        }
+
+    }
+
+    public boolean doubleBet(MenuItem view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        startActivity(intent);
+        return true;
+    }
+
 
 }
